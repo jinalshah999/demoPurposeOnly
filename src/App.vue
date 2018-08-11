ce0fba7d5b9f9965b54206015ff2fe97ea87ad4e
jinalshah999 <jinalshah999@gmail.com>
2018-08-11 22:03:02 +0530

    http-crud demo

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <app-todo></app-todo>
+  </div>
+</template>
+
+<script>
+export default {
+data(){
+  return{
+    msg:"hello"
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
